4531ad7018b0532c2965d52c5e1d3df714dca230
sepy97 <sepy97@gmail.com>
2021-04-26 20:37:47 -0700

    trivial matrix multiplication

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 
 int main ()
 {
+	int N = 100;
+	int  A[N][N], B[N][N], C[N][N];
+	
+	for (int i = 0; i < N-1; i++)
+	{
+		for (int j = 0; i < N - 1; i++)
+		{
+			for (int k = 0; i < N - 1; i++)
+			{
+				C[i][j] = C[i][j] + A[i][k] * B[k][j];
+				
+				
+			}
+		}
+	}
+	
+	printf ("%d \n", A[5][7]);
 	printf ("Hello, World!\n");
+	
+	
 	return 0;
 }
